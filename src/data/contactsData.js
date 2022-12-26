8ca4364eea88b251b41b1e6e5ce6d614992f564d
adarsh-2906 <adarshsoni2906@gmail.com>
2022-12-26 12:40:58 +0530

    updated

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
+    email: 'adarshsoni2906@gmail.com',
+    phone: '+919131544756',
     address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
 
     sheetAPI: ''
