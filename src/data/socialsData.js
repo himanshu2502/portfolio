b85c3925caa98bb598d9de3a04f4451a904d1b91
adarsh-2906 <adarshsoni2906@gmail.com>
2022-12-26 13:03:10 +0530

    add

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,9 +1,6 @@
 export const socialsData = {
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    medium: 'https://medium.com/@abusaid-bd',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    mail: 'abusaid7388@gmail.com',
+    github: 'https://github.com/adarsh-2906',
+    facebook: 'https://www.facebook.com/adarshnagvanshi/',
+    linkedIn: 'https://www.linkedin.com/in/adarshsoni29/',
+    mail: 'adarshsoni2906@gmail.com',
 }
