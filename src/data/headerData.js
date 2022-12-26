25d0ce23f71e940b71c1342e8528b3a5b72ece22
adarsh-2906 <adarshsoni2906@gmail.com>
2022-12-26 13:24:55 +0530

    okk

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 
 export const headerData = {
-    name: 'ADARSH SONI',
+    name: 'ADARSH',
     title: "Hello! I am",
     desciption: "Hello! I am Adarsh Soni, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'file:///E:/sonam%20ppt/IMG_20220616_121404.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    imagebw: 'file:///E:/sonam%20ppt/IMG_20220616_121404.jpg',
     resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
 }
