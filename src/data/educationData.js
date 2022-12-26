36a0f67e7e177d112721671a66aa6714fa584940
adarsh-2906 <adarshsoni2906@gmail.com>
2022-12-26 12:50:42 +0530

    okk

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,24 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Noakhali Government College',
+        institution: 'VIT Bhopal University',
         course: 'Bachelor Degree',
-        startYear: '2020',
-        endYear: 'Present'
+        startYear: '2019',
+        endYear: 'Present',
+        Score : '81.5%'
     },
     {
         id: 2,
-        institution: 'Noakhali Islamia Kamil Madrasah',
-        course: 'Higher Secondary',
+        institution: 'Morition Public School',
+        course: 'Higher Secondary School',
         startYear: '2018',
         endYear: '2020'
     },
     {
         id: 3,
-        institution: 'Baitus Saif Islamia Madrasah',
+        institution: 'Morition Public School',
         course: 'Secondary School',
-        startYear: '2008',
-        endYear: '2018'
+        startYear: '2015',
+        endYear: '2016'
     },
 ]
