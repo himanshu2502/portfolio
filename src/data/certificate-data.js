76c87c3a6525a3a9bbf1b07dcd5d56c324ef712b
adarsh-2906 <adarshsoni2906@gmail.com>
2023-01-08 22:00:57 +0530

    add

--- src/data/certificate-data.js
+++ src/data/certificate-data.js
@@ -1,89 +1,89 @@
-import certificate1 from "../assets/certificate/certificate01.jpg";
+// import certificate1 from "../assets/certificate/certificate01.jpg";
 import certificate2 from "../assets/certificate/certificate02.jpg";
-import certificate3 from "../assets/certificate/certificate03.png";
-import certificate4 from "../assets/certificate/certificate04.png";
+// import certificate3 from "../assets/certificate/certificate03.png";
+// import certificate4 from "../assets/certificate/certificate04.png";
 import certificate5 from "../assets/certificate/certificate05.png";
 import certificate6 from "../assets/certificate/certificate06.png";
 import certificate7 from "../assets/certificate/certificate07.jpg";
 import certificate8 from "../assets/certificate/certificate08.jpg";
 import certificate9 from "../assets/certificate/certificate09.jpg";
-import certificate10 from "../assets/certificate/certificate10.png";
-import certificate11 from "../assets/certificate/certificate11.jpg";
-import certificate12 from "../assets/certificate/certificate12.jpg";
+// import certificate10 from "../assets/certificate/certificate10.png";
+// import certificate11 from "../assets/certificate/certificate11.jpg";
+// import certificate12 from "../assets/certificate/certificate12.jpg";
 
 export const certificateData = [
   {
     id: "02",
-    title: "Advanced SQL",
+    title: "Microsoft Azure-900 ",
     image: certificate2,
-    link: "https://olympus1.mygreatlearning.com/course_certificate/JPVHJLVW",
+    link: "https://i.ibb.co/ZGVZXyX/azure-certificate.jpg",
   },
 
   {
     id: "05",
-    title: "Succeeding in Full Stack and Front End",
+    title: "GWOC",
     image: certificate5,
-    link: "https://www.linkedin.com/learning/certificates/6a8e1e26ca1461fa8de47a87d06e9f6c0d0fafcc5cc53b1b7b8fb2a0437a1ecc",
+    link: "https://i.ibb.co/Lr5v252/GWOC-PARTICIPATION-CERTIFICATE-1.jpg",
   },
 
   {
     id: "07",
-    title: "Web Scraping with Python",
+    title: "Web Developer",
     image: certificate7,
-    link: "https://olympus1.greatlearning.in/course_certificate/BCLHKCGB",
+    link: "https://i.ibb.co/HHPMmgj/UC-e7bff9e2-8b6a-42e4-99ab-346306b9d5e5.jpg",
   },
   {
     id: "08",
-    title: "Oracle SQL",
+    title: "Problem Solving Basic",
     image: certificate8,
-    link: "https://olympus1.mygreatlearning.com/course_certificate/NMXQFEGR",
+    link: "https://i.ibb.co/t3HxQ2R/download-1.png",
   },
   {
     id: "09",
-    title: "SQL Projects for Beginners",
+    title: "Problem Solving Intermediate",
     image: certificate9,
-    link: "https://olympus1.mygreatlearning.com/course_certificate/BZTLLLBC",
+    link: "https://i.ibb.co/W3Sc5yw/download-2.png",
   },
   {
     id: "06",
-    title: "Programming Foundations: Databases",
+    title: "R Basic",
     image: certificate6,
-    link: "https://www.linkedin.com/learning/certificates/e4af56f73e73d062725de61ad945d2d87c6387947ab36365002bb55856aee1ec",
+    link: "https://i.ibb.co/QCK9bWd/download.png",
   },
-  {
-    id: "12",
-    title: "Complete web development with Programming hero",
-    image: certificate12,
-    link: "https://drive.google.com/file/d/1e_AdM2-6f_nAfn6NXNXONDdSyNW-FZ1x/view",
-  },
-  {
-    id: "01",
-    title: "ReactJS for Beginners",
-    image: certificate1,
-    link: "https://certificates.simplicdn.net/share/2995717_1636118401.pdf",
-  },
-  {
-    id: "10",
-    title: " Theoretical and practical Javascript",
-    image: certificate10,
-    link: "https://www.sololearn.com/Certificate/CT-GTM1MUJ4/png",
-  },
-  {
-    id: "03",
-    title: "Programming Foundations: Fundamentals",
-    image: certificate3,
-    link: "https://www.linkedin.com/learning/certificates/c506fccfaaeba792cfde6ad89a10df65ece487250c8603a7e66f720739b1bbcf",
-  },
-  {
-    id: "11",
-    title: "Theoretical and practical HTML",
-    image: certificate11,
-    link: "https://www.sololearn.com/Certificate/CT-HCDBVC4Y/jpg",
-  },
-  {
-    id: "04",
-    title: "HTML Essential Training",
-    image: certificate4,
-    link: "https://www.linkedin.com/learning/certificates/d43d60fba3892a79f0ddd18a574fe4f97a9c89e52bc98734dd3cf8ebe3f300e8",
-  },
+  // {
+  //   id: "12",
+  //   title: "Complete web development with Programming hero",
+  //   image: certificate12,
+  //   link: "https://drive.google.com/file/d/1e_AdM2-6f_nAfn6NXNXONDdSyNW-FZ1x/view",
+  // },
+  // {
+  //   id: "01",
+  //   title: "ReactJS for Beginners",
+  //   image: certificate1,
+  //   link: "https://certificates.simplicdn.net/share/2995717_1636118401.pdf",
+  // },
+  // {
+  //   id: "10",
+  //   title: " Theoretical and practical Javascript",
+  //   image: certificate10,
+  //   link: "https://www.sololearn.com/Certificate/CT-GTM1MUJ4/png",
+  // },
+  // {
+  //   id: "03",
+  //   title: "Programming Foundations: Fundamentals",
+  //   image: certificate3,
+  //   link: "https://www.linkedin.com/learning/certificates/c506fccfaaeba792cfde6ad89a10df65ece487250c8603a7e66f720739b1bbcf",
+  // },
+  // {
+  //   id: "11",
+  //   title: "Theoretical and practical HTML",
+  //   image: certificate11,
+  //   link: "https://www.sololearn.com/Certificate/CT-HCDBVC4Y/jpg",
+  // },
+  // // {
+  // //   id: "04",
+  // //   title: "HTML Essential Training",
+  // //   image: certificate4,
+  // //   link: "https://www.linkedin.com/learning/certificates/d43d60fba3892a79f0ddd18a574fe4f97a9c89e52bc98734dd3cf8ebe3f300e8",
+  // // },
 ];
