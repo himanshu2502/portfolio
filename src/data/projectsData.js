c092fd49ce32dd14b36804a63610985d24996c75
adarsh-2906 <adarshsoni2906@gmail.com>
2023-01-08 22:15:09 +0530

    ok

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,76 +11,77 @@ import two from '../assets/svg/projects/two.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
+        projectName: 'Blogger Hub',
+        projectDesc: 'Blogging Website with full CRUD functionality with Authentication and Authorization.',
+        tags: [HTML/CSS, JavaScript, bootstrap],
         code: 'https://github.com/adarsh-2906',
         demo: 'https://github.com/adarsh-2906',
         image: six
     },
     {
         id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
+        projectName: 'Face Mask Recognition Software',
+        projectDesc: 'Face Mask Recognition Software',
+        tags: [Python, AI and ML],
         code: 'https://github.com/adarsh-2906',
         demo: 'https://github.com/adarsh-2906',
         image: seven
     },
     {
         id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/said7388/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
+        projectName: 'Word Count Visualizer',
+        projectDesc: 'Implemented an showed the working of Zipf’s Law & Heap’s Law for scrapping the entered
+        Text in the form of Graph. Implemented Graph JS in a Web App.',
+        tags: [HTML/CSS, Graph.Js],
+        code: 'https://github.com/adarsh-2906',
+        demo: 'https://github.com/adarsh-2906',
         image: eight
     },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
-    },
+    // {
+    //     id: 4,
+    //     projectName: 'Android Patient Tracker',
+    //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+    //     tags: ['Flutter', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: four
+    // },
+    // {
+    //     id: 5,
+    //     projectName: 'E-Commerce App',
+    //     projectDesc: 'A Simple E-commerce application',
+    //     tags: ['React Native', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: five
+    // },
+    // {
+    //     id: 6,
+    //     projectName: 'Uber Lite',
+    //     projectDesc: 'Uber clone',
+    //     tags: ['Flutter'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: one
+    // },
+    // {
+    //     id: 7,
+    //     projectName: 'Stock Market App',
+    //     projectDesc: 'A simple stock market API app',
+    //     tags: ['React', 'Redux', 'Bootstrap'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: two
+    // },
+    // {
+    //     id: 8,
+    //     projectName: 'Car Pooling System',
+    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+    //     tags: ['Flutter', 'React'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: three
+    // },
 ]
 
 
