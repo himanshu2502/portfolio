01061a4eeb9fe5ed4105bb486d0c3734d11b12df
himanshu2502 <66895895+himanshu2502@users.noreply.github.com>
2023-01-17 17:21:23 +0600

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
-    // {
-    //     id: 1,
-    //     company: 'Teton Private Limited',
-    //     jobtitle: 'Software Engineer I',
-    //     startYear: 'Jan 2022',
-    //     endYear: 'Present'
-    // },
+    {
+        id: 1,
+        company: 'Make My Trip',
+        jobtitle: 'Software Engineer Intern',
+        startYear: 'Sep 2022',
+          endYear: 'Present'
+     },
     {
         id: 2,
-        company: 'GWOC',
+        company: 'Tathastu DSA Scholar',
         jobtitle: 'Contributor',
-        startYear: 'Sep 2021',
-        endYear: 'Nov 2021'
-    },
-    {
-        id: 3,
-        company: 'Learning New Technology For Everyday',
-        jobtitle: 'Self Learning',
-        startYear: '2019',
-        endYear: 'Present'
+        startYear: 'Jul 2021',
+        endYear: 'Aug 2021'
     }
+//     {
+//         id: 3,
+//         company: 'Learning New Technology For Everyday',
+//         jobtitle: 'Self Learning',
+//         startYear: '2019',
+//         endYear: 'Present'
+//     }
 ]
