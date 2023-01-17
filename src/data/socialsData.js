332475c4825b0365396520836746283cc216cd51
himanshu2502 <66895895+himanshu2502@users.noreply.github.com>
2023-01-17 17:40:19 +0600

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,7 +1,7 @@
 export const socialsData = {
-    github: 'https://github.com/adarsh-2906',
-    facebook: 'https://www.facebook.com/adarshnagvanshi/',
-    linkedIn: 'https://www.linkedin.com/in/adarshsoni29/',
+    github: 'http://github.com/himanshu2502',
+    facebook: 'https://www.facebook.com/profile.php?id=100009514500866',
+    linkedIn: 'https://www.linkedin.com/in/himanshu-dalal-084486195',
     twitter : 'https://www.twitter.com',
-    mail: 'adarshsoni2906@gmail.com'
+    mail: 'anshudalal748@gmail.com'
 }
