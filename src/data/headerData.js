bc4327719763cca38be1592193d18a641dbb1164
himanshu2502 <66895895+himanshu2502@users.noreply.github.com>
2023-01-17 17:28:53 +0600

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 
 export const headerData = {
-    name: 'Adarsh Soni',
+    name: 'Himanshu Dalal',
     title: "Hello! I am",
-    desciption: "Hello! I am Adarsh Soni, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/GsPwP11/IMG-20220616-121404.jpg',
-    imagebw: 'https://i.ibb.co/GsPwP11/IMG-20220616-121404.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1ILHThqKPorcXpMqMynelHi_oAme84fYv/view?usp=share_link'
+    desciption: "Hello! I am Himanshu Dalal, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    image: 'https://i.ibb.co/PQc2FYc/Whats-App-Image-2023-01-17-at-3-45-04-PM.jpg',
+    imagebw: 'https://i.ibb.co/PQc2FYc/Whats-App-Image-2023-01-17-at-3-45-04-PM.jpg',
+    resumePdf: 'https://drive.google.com/file/d/1NMvuSan-syz7IJXDVPc0bs9_7gkF5d5Q/view?usp=drivesdk'
 }
