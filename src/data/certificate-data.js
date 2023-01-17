dd31439aa3e3b69ac3d4d64804915d7dba06bbb0
adarsh-2906 <adarshsoni2906@gmail.com>
2023-01-17 11:45:03 +0530

    okk

--- src/data/certificate-data.js
+++ src/data/certificate-data.js
@@ -2,11 +2,11 @@
 import certificate2 from "../assets/certificate/certificate02.jpg";
 // import certificate3 from "../assets/certificate/certificate03.png";
 // import certificate4 from "../assets/certificate/certificate04.png";
-import certificate5 from "../assets/certificate/certificate05.png";
+import certificate5 from "../assets/certificate/certificate05.jpg";
 import certificate6 from "../assets/certificate/certificate06.png";
 import certificate7 from "../assets/certificate/certificate07.jpg";
-import certificate8 from "../assets/certificate/certificate08.jpg";
-import certificate9 from "../assets/certificate/certificate09.jpg";
+import certificate8 from "../assets/certificate/certificate08.png";
+import certificate9 from "../assets/certificate/certificate09.png";
 // import certificate10 from "../assets/certificate/certificate10.png";
 // import certificate11 from "../assets/certificate/certificate11.jpg";
 // import certificate12 from "../assets/certificate/certificate12.jpg";
