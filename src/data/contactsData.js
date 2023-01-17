1b6e08c1facbabd91d3a253994e635115d0465ee
himanshu2502 <66895895+himanshu2502@users.noreply.github.com>
2023-01-17 17:32:24 +0600

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'adarshsoni2906@gmail.com',
-    phone: '+919131544756',
-    address: 'Rewa, Madhya pradesh , india -486661',
+    phone: '+918250773487',
+    address: 'Dharampal Singh , Village Rewarikhera, VPO Rewarikhera, Dist. Jhajjar , tehsil Bahadurgarh, Haryana - 124504',
 
     sheetAPI: ''
 }
